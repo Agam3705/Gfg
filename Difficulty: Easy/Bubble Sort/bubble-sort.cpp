7bb5ebf9b3f330f560e060a96427235d35f2e09a
Agam Jindal <134130506+Agam3705@users.noreply.github.com>
2026-07-23 00:52:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bubble Sort/bubble-sort.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    void bubbleSort(vector<int>& arr) {
+        int n = arr.size();
+        for(int i =0; i<n;i++){
+            for(int j =i+1;j<n;j++){
+                if(arr[i]>arr[j]){
+                    swap(arr[i],arr[j]);
+                }
+            }
+        }
+        
+    }
+};
