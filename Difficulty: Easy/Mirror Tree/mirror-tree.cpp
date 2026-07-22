5962b59713261490efbca3c4bd28a405f2705e82
Agam Jindal <134130506+Agam3705@users.noreply.github.com>
2026-07-23 00:48:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Mirror Tree/mirror-tree.cpp
@@ -0,0 +1,26 @@
+/*
+class Node {
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int x) {
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    void mirror(Node* root) {
+        if(root==NULL) return;
+        mirror(root->left);
+        mirror(root->right);
+        Node* temp = root->left;
+        root->left = root->right;
+        root->right= temp;
+        
+    }
+};
